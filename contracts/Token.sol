5fdfe153dd251de12b5e0d6faace2d07e7a79163
hgaskin <37593078+hgaskin@users.noreply.github.com>
2022-08-09 23:11:23 -0400

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,9 +10,13 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    // Track Balances
+    mapping(address => uint256) public balanceOf;
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals); // 500 DAPP tokens (500 x 10^18 = 500 DAPP tokens)
+        balanceOf[msg.sender] = totalSupply;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,14 @@ const tokens = (n) => {
 };
 
 describe("Token Contract Test", () => {
-  let token;
+  let token, accounts, deployer;
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token");
     token = await Token.deploy("Dapp University", "DAPP", 500);
+
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
   });
 
   describe("Deployment", () => {
@@ -33,6 +36,10 @@ describe("Token Contract Test", () => {
 
     it("Has correct total supply", async () => {
       expect(await token.totalSupply()).to.equal(totalSupply);
+    });
+
+    it("Assigns total supply to deployer", async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
     });
   });
 });
