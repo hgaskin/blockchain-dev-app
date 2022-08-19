061ce3e34d4a16d79c28a3cfb9932de93c5fc673
hgaskin <37593078+hgaskin@users.noreply.github.com>
2022-08-18 23:47:21 -0400

    7. Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,11 +14,7 @@ contract Token {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 value
-    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     event Approval(
         address indexed owner,
@@ -27,39 +23,48 @@ contract Token {
     );
 
     constructor(
-        string memory _name, 
-        string memory _symbol, 
+        string memory _name,
+        string memory _symbol,
         uint256 _totalSupply
-    ){
+    ) {
         name = _name;
         symbol = _symbol;
-        totalSupply = _totalSupply * (10 ** decimals); // 500 DAPP tokens (500 x 10^18 = 500 DAPP tokens)
+        totalSupply = _totalSupply * (10**decimals); // 500 DAPP tokens (500 x 10^18 = 500 DAPP tokens)
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) 
+    function transfer(address _to, uint256 _value)
         public
-        returns (bool success) 
+        returns (bool success)
     {
-    // Check to confirm that the sender has enough balance to send
-    require(balanceOf[msg.sender] >= _value);
-    require(_to != address(0));
+        // Check to confirm that the sender has enough balance to send
+        require(balanceOf[msg.sender] >= _value);
 
-    // Deduct tokens from sender
-     balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        _transfer(msg.sender, _to, _value);
 
-    //  Credit tokens to receiver
-     balanceOf[_to] = balanceOf[_to] + _value;
-     
-    //  Emit an event for the transfer
-     emit Transfer(msg.sender, _to, _value);
-
-     return true;
+        return true;
     }
 
-    function approve(address _spender, uint256 _value) 
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        // Deduct tokens from sender
+        balanceOf[_from] = balanceOf[_from] - _value;
+
+        // Credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        // Emit an event for the transfer
+        emit Transfer(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value)
         public
-        returns (bool success) 
+        returns (bool success)
     {
         // Check to confirm reject invalid spender
         require(_spender != address(0));
@@ -67,6 +72,24 @@ contract Token {
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // Check to confirm that the sender has enough balance to send
+        require(_value <= balanceOf[_from]);
+        // Check for approval from the sender
+        require(_value <= allowance[_from][msg.sender]);
+
+        // Reset Allowance to prevent reentrancy attacks
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+        _transfer(_from, _to, _value);
+
         return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -137,4 +137,58 @@ describe("Token Contract Test", () => {
       });
     });
   });
+
+  describe("Delegated Token Transfers", () => {
+    let amount, transaction, result;
+
+    beforeEach(async () => {
+      amount = tokens("100");
+      transaction = await token
+        .connect(deployer)
+        .approve(exchangeAddress, amount);
+      result = await transaction.wait();
+    });
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        transaction = await token
+          .connect(exchange)
+          .transferFrom(deployerAddress, receiverAddress, amount);
+        result = await transaction.wait();
+      });
+
+      it("transfers token balances", async () => {
+        // Log balance before transfer
+        expect(await token.balanceOf(deployerAddress)).to.equal(tokens("400"));
+        expect(await token.balanceOf(receiverAddress)).to.equal(amount);
+      });
+
+      it("resets the allowance to prevent reentrancy attacks", async () => {
+        expect(
+          await token.allowance(deployerAddress, exchangeAddress)
+        ).to.equal(tokens("0"));
+      });
+
+      it("emits transfer event", async () => {
+        const event = result.events[0];
+        expect(event.event).to.equal("Transfer");
+
+        const args = event.args;
+        expect(args.from).to.equal(deployerAddress);
+        expect(args.to).to.equal(receiverAddress);
+        expect(args.value).to.equal(amount);
+      });
+    });
+
+    describe("Failure", () => {
+      const invalidAmount = tokens("1000000");
+      it("reverts the attempt to transfer value that exceeds approval", async () => {
+        await expect(
+          token
+            .connect(exchange)
+            .transferFrom(deployerAddress, receiverAddress, invalidAmount)
+        ).to.be.reverted;
+      });
+    });
+  });
 });
